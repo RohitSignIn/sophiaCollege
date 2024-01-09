c024e82767e7f1b6642a959ad461d3d798e7b593
RohitSignIn <rohitsinghsignin@gmail.com>
2024-01-09 15:52:18 +0530

    python

--- GUI/TicTacToe.py
+++ GUI/TicTacToe.py
@@ -13,10 +13,15 @@ MainFrame.grid(row=0, column=0, sticky="ewns")
 MainFrame.grid_columnconfigure(0, weight=1)
 MainFrame.grid_rowconfigure(0, weight=1)
 
+# global 
 playerTurn = "x"
+movesDone = {}
+winner = False
 
 def handleBtnClick(btnNum):
     global playerTurn
+
+    movesDone[btnNum] = playerTurn
 
     btn = Button(tictactoeFrame, width=8, height=4, text=playerTurn, fg="#2d2d2d", bg="#fff")
 
@@ -27,11 +32,43 @@ def handleBtnClick(btnNum):
     elif(btnNum >= 6 and btnNum < 9):
         btn.grid(row=2, column=btnNum-6)
 
+    isWinner(playerTurn)
 
-    if(playerTurn == "x"):
-        playerTurn = "o"
+
+def isWinner(turn):
+    global winner
+    global playerTurn
+
+    winnerList = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+        ]
+    
+    for i in winnerList:
+        winner = True
+        for j in i:
+            if(i in movesDone[j]):
+                if(movesDone[j] != turn):
+                    winner = False
+                    break
+                winner = False
+                break
+        if(winner):
+            break
+
+    if(winner):
+        print("Got Winner", turn)
     else:
-        playerTurn = "x"
+        if(playerTurn == "x"):
+            playerTurn = "o"
+        else:
+            playerTurn = "x"
 
 
 def initialStruc():
